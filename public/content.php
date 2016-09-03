b63e78dd220cabb73166649491335eb38dfc52f5
Diana Thompson <likethegoddess@gmail.com>
2016-09-03 10:57:52 -0700

    Add metadata to single template
    Plus supporting CSS changes to type.less/style(.min).css

--- public/content.php
+++ public/content.php
@@ -4,6 +4,9 @@
 			<figure><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail', array( 'class' => 'img-responsive' ) ); ?></a></figure> 
 		<?php } ?> 
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<?php if ( is_single() ) { ?>	
+			<div class="metadata"><?php likethebootstrap_entry_meta(); ?></div>	
+		<?php } ?>		
 		<?php if ( is_home() ) { ?>	
 			<?php the_excerpt(); ?>
 		<?php } else { ?>		
