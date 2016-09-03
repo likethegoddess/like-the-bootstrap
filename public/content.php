17e5788ffade547d9a5df3d518c91d6812db1e8b
Diana Thompson <likethegoddess@gmail.com>
2016-09-03 12:13:41 -0700

    Add featured image support to page, page-full, and single templates

--- public/content.php
+++ public/content.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="entry-content">
-		<?php if ( has_post_thumbnail() && !is_single() ) { ?>
-			<figure><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail', array( 'class' => 'img-responsive' ) ); ?></a></figure> 
+		<?php if ( has_post_thumbnail() && !is_page() ) { ?>
+			<figure><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('', array( 'class' => 'img-responsive' ) ); ?></a></figure> 
 		<?php } ?> 
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		<?php if ( is_single() ) { ?>	
