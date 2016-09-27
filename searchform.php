c784caf1a6bfdedff4593e4a5cd1c10a09685167
Diana Thompson <likethegoddess@gmail.com>
2016-09-26 21:45:10 -0700

    Adjust search form/button padding and remove search form screen reader class

--- searchform.php
+++ searchform.php
@@ -8,5 +8,5 @@
 	<label>
 		<input type="search" class="search-field" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'like-the-bootstrap' ); ?></span></button>
+	<button type="submit" class="search-submit"><?php echo _x( 'Search', 'submit button', 'likethebootstrap' ); ?></button>
 </form>
